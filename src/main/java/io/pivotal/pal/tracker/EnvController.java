4f868b0b0f25ce07a2a5ee74f6877419c52eba15
eric <ericmerrell@yahoo.com>
2020-12-15 22:09:07 +0000

    update for push

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -11,7 +11,9 @@ import java.util.Map;
 public class EnvController {
 
     private final String port;
+
     private final String memoryLimit;
+    
     private final String cfInstanceIndex;
     private final String cfInstanceAddress;
 
